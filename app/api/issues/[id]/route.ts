4459221df06aa0f8d674c417c70023a36b90dced
Fahleh <eddyukpong@gmail.com>
2024-11-19 18:17:47 +0100

    Refactor: Enchanced api to update issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,7 @@
 import authOptions from '@/app/auth/authOptions';
-import { issueSchema } from '@/app/validationSchema';
+import { patchIssueSchema } from '@/app/validationSchema';
 import prisma from '@/prisma/client';
+import { Status } from '@prisma/client';
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -8,12 +9,12 @@ export async function PATCH(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) {
-  const session = await getServerSession(authOptions);
-  if (!session) return NextResponse.json({}, { status: 401 });
+  // const session = await getServerSession(authOptions);
+  // if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
 
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
@@ -53,6 +54,19 @@ export async function DELETE(
 }
 
 const updateIssue = async (id: string, body: any) => {
+  const { title, description, status, assignedToUserId } = body;
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+
+    if (!user)
+      return NextResponse.json({ error: 'Invalid User' }, { status: 400 });
+  }
+
+  // if (status !== typeof Status)
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id) },
   });
@@ -64,8 +78,10 @@ const updateIssue = async (id: string, body: any) => {
     const updatedIssue = await prisma.issue.update({
       where: { id: issue.id },
       data: {
-        title: body.title,
-        description: body.description,
+        title,
+        description,
+        status,
+        assignedToUserId,
       },
     });
 
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,6 +1,14 @@
+import { Status } from '@prisma/client';
 import { z } from 'zod';
 
 export const issueSchema = z.object({
   title: z.string().min(3).max(255),
   description: z.string().min(4),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(3).max(255).optional(),
+  description: z.string().min(4).max(65535).optional(),
+  status: z.nativeEnum(Status).optional(),
+  assignedToUserId: z.string().min(1).max(255).optional().nullable(),
 });
