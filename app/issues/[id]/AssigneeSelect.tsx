3e3040c8c7987f26c13b5cb5e6b76d0281fc7ee3
Fahleh <eddyukpong@gmail.com>
2024-11-19 17:10:04 +0100

    Refactor: Replaced useEffect api call with Reacr Query.

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,49 +1,38 @@
 'use client';
 
+import { Skeleton } from '@/app/components';
 import { User } from '@prisma/client';
-import { Select } from '@radix-ui/themes';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then((response) => response.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>('/api/users');
+  if (isLoading) return <Skeleton height="2rem" />;
 
-      if (data) setUsers(data);
-    };
-
-    fetchUsers();
-  }, []);
+  if (error) return null;
 
   return (
-    <select name="" id="" className='border-2 rounded-md p-1 bg'>
+    <select name="" id="" className="border-2 rounded-md p-1 bg">
       <option value="">Assign...</option>
-      {users.map((user) => (
-            <option key={user.id} value={user.id}>
-              {user.name}
-            </option>
-          ))}
+      <option value="" disabled>
+        Suggestions
+      </option>
+      {users?.map((user) => (
+        <option key={user.id} value={user.id}>
+          {user.name}
+        </option>
+      ))}
     </select>
-    //  <p></p>
-    // <Select.Root>
-    //   <Select.Trigger />
-    //   <Select.Content>
-    //     <Select.Group>
-    //       <Select.Item value="placeholder" disabled>
-    //         Assign..
-    //       </Select.Item>
-    //       <Select.Label>Suggestions</Select.Label>
-    //       {users.map((user) => (
-    //         <Select.Item key={user.id} value={user.id}>
-    //           {user.name}
-    //         </Select.Item>
-    //       ))}
-    //     </Select.Group>
-    //   </Select.Content>
-    // </Select.Root>
   );
 };
 
