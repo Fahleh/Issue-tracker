b043b229c4d8adfe0cfa3b74766fbe57cc8de73a
Fahleh <eddyukpong@gmail.com>
2024-11-19 14:52:11 +0100

    Feat(authGuard): Built middleware and protected routes

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,12 +5,16 @@ import { notFound } from 'next/navigation';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
 import DeleteIssueButton from './DeleteIssueButton';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOptions';
 
 interface RouteParams {
   params: Promise<{ id: string }>;
 }
 
 const IssueDetailsPage = async ({ params }: RouteParams) => {
+  const session = await getServerSession(authOptions);
+
   const { id } = await params;
 
   const issue = await prisma.issue.findUnique({
@@ -23,14 +27,16 @@ const IssueDetailsPage = async ({ params }: RouteParams) => {
 
   return (
     <Grid columns={{ initial: '1', sm: '5' }} gap="5">
-      <Box className='md:col-span-4'>
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Flex direction="column" gap='4'>
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
+        {session && (
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        )}
       </Box>
     </Grid>
   );
