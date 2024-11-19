94477ce29260d3dc511a65e7b468546647eb87ee
Fahleh <eddyukpong@gmail.com>
2024-11-19 19:03:35 +0100

    Chore: Added toast notifcations

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -5,6 +5,7 @@ import { Issue, User } from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { ChangeEvent } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -22,32 +23,57 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 
   if (error) return null;
 
+  const toastOptions = {
+    success: {
+      style: {
+        background: '#32CD32',
+        color: 'white',
+      },
+    },
+    error: {
+      style: {
+        background: '#dc3545',
+        color: 'white',
+      },
+    },
+  }
+
   const assignIssue = (event: ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
 
-    axios.patch('/api/issues/' + issue.id, {
-      assignedToUserId: value || null,
-    });
+    axios
+      .patch('/xapi/issues/' + issue.id, {
+        assignedToUserId: value || null,
+      })
+      .then(() => toast.success(`Issue assigned successfully.`))
+      .catch(() => {
+        toast.error('Changes could not be saved.');
+      });
   };
 
   return (
-    <select
-      onChange={assignIssue}
-      defaultValue={issue.assignedToUserId || ''}
-      name=""
-      id=""
-      className="border-2 rounded-md p-1 bg"
-    >
-      <option value="">Unassigned</option>
-      <option value="" disabled>
-        Suggestions
-      </option>
-      {users?.map((user) => (
-        <option key={user.id} value={user.id}>
-          {user.name}
+    <>
+      <select
+        onChange={assignIssue}
+        defaultValue={issue.assignedToUserId || ''}
+        name=""
+        id=""
+        className="border-2 rounded-md p-1 bg"
+      >
+        <option value="">Unassigned</option>
+        <option value="" disabled>
+          Suggestions
         </option>
-      ))}
-    </select>
+        {users?.map((user) => (
+          <option key={user.id} value={user.id}>
+            {user.name}
+          </option>
+        ))}
+      </select>
+      <Toaster
+        toastOptions={toastOptions}
+      />
+    </>
   );
 };
 
