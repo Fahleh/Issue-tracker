48582075ad8a2d8612851e07476bdcdf908de1a5
Fahleh <eddyukpong@gmail.com>
2024-11-22 17:15:42 +0100

    Feat: Built LatestIssues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Pagination from './components/Pagination';
+import LatestIssues from './LatestIssues';
 
-export default async  function Home({ searchParams }: { searchParams: Promise<{ page: string }>}) {
-  const params = await searchParams;
-
-  return (
-    <div>
-      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(params.page)} />{' '}
-    </div>
-  );
+export default function Home() {
+  return <LatestIssues />;
 }
